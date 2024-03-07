df36e4e408d2c6ea755eaad9c1477a295b00abfb
Peter Doronin <109960979+peter-doronin@users.noreply.github.com>
2024-03-07 14:45:16 +0300

    Update comm.c

--- comm.c
+++ comm.c
@@ -1,1 +1,8 @@
-
+unsigned char crc(unsigned char * data, size_t size){
+  if(size == 0) return 0;
+  uinsigned char result = 0;
+  for(int i = 0; i < size; i++){
+    result ^= data[i]; 
+  }
+  return result;
+}
