c67a64ecd3674e5af8db1f636710390a330883ac
Peter Doronin <109960979+peter-doronin@users.noreply.github.com>
2024-03-07 15:37:20 +0300

    Update comm.c

--- comm.c
+++ comm.c
@@ -1,3 +1,5 @@
+#define ADDR_T uint8_t;
+
 unsigned char crc(unsigned char * data, size_t size){
   if(size == 0) return 0;
   uinsigned char result = 0;
@@ -5,4 +7,10 @@ unsigned char crc(unsigned char * data, size_t size){
     result ^= data[i]; 
   }
   return result;
+}
+
+int comm_send_msg(ADDR_T src, ADDR_T dst, uinsigned char * data, size_t size){
+}
+
+int comm_reciev_msg(ADDR_T src, ADDR_T dst, uinsigned char * data, size_t size, int timeout){
 }
