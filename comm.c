f5f56756a6c6634d6fd889030230b893e864e731
Peter Doronin <109960979+peter-doronin@users.noreply.github.com>
2024-03-11 20:26:30 +0300

    Update comm.c

--- comm.c
+++ comm.c
@@ -1,3 +1,5 @@
+#include "comm.h"
+
 #define ADDR_T uint8_t;
 
 unsigned char crc(unsigned char * data, size_t size){
@@ -8,9 +10,27 @@ unsigned char crc(unsigned char * data, size_t size){
   }
   return result;
 }
+struct msg_s {
+char preamble[4];
+char length;
+char src;
+char dst;
+char data[];
+};
 
-int comm_send_msg(ADDR_T src, ADDR_T dst, uinsigned char * data, size_t size){
+const char preamble[] = {0x01, 0x02, 0x03, 0x04};
+
+int comm_send_msg(IO io_handle, ADDR_T src, ADDR_T dst, char * data, int size){
 }
 
-int comm_reciev_msg(ADDR_T src, ADDR_T dst, uinsigned char * data, size_t size, int timeout){
+int comm_recieve_msg(IO io_handle, ADDR_T src, ADDR_T dst, char * data, int size, int timeout){
+  int start_time = os_gettime_milliseconds();
+  int recieve = 1;
+  int timeout_elapsed = 0;
+  while(recieve){
+    timeout_elapsed = os_gettime_milliseconds() - start_time;
+    if(timeout_elapsed > timeout) return COMM_TIMEOUT;
+    int err = read(io_handle, data, size, timeout_elapsed);
+    if(err == IO_TIMEOUT) return COMM_TIMEOUT;
+    if(err < 0) return COMM_ERROR;
 }
